6f32ffe68181dddf40025dcafd76d8c2b01cd17f
rpashish <120458689+rpashish@users.noreply.github.com>
2022-12-17 10:48:32 +0530

    Add files via upload

---
+++ ForexServiceNamingEurekaServer/src/test/java/com/ashish/sprint/tutorials/naming/server/ForexServiceNamingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.sprint.tutorials.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexServiceNamingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
