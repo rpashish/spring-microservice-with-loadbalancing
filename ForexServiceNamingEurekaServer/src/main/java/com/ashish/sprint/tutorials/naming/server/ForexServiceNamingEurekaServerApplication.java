6b60139673a053edbcb2aa370f58d08101f8fde0
rpashish <120458689+rpashish@users.noreply.github.com>
2022-12-17 10:58:16 +0530

    Add files via upload

---
+++ ForexServiceNamingEurekaServer/src/main/java/com/ashish/sprint/tutorials/naming/server/ForexServiceNamingEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ashish.sprint.tutorials.naming.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ForexServiceNamingEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceNamingEurekaServerApplication.class, args);
+	}
+
+}
